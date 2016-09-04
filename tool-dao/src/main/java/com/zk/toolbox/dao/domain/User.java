f4b55f6eb02fbceb01273c61df3e654d493287fc
jinyuan <jinyuan@alibaba-inc.com>
2016-09-04 22:19:36 +0800

    代码提交

---
+++ tool-dao/src/main/java/com/zk/toolbox/dao/domain/User.java
@@ -0,0 +1,38 @@
+package com.zk.toolbox.dao.domain;
+
+/**
+ * Created by zhengke on 16/9/4.
+ */
+public class User {
+
+
+    private int uid;
+
+    private String data;
+
+    private String userName;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
