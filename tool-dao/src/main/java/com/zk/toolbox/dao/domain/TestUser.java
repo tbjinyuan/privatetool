11f980b9d2dd9f1b25f6ccc874b579a99014fc61
jinyuan <jinyuan@alibaba-inc.com>
2016-09-05 18:27:30 +0800

    all commit

---
+++ tool-dao/src/main/java/com/zk/toolbox/dao/domain/TestUser.java
@@ -0,0 +1,11 @@
+package com.zk.toolbox.dao.domain;
+
+/**
+ * Created by zhengke on 16/9/5.
+ */
+public class TestUser {
+
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
