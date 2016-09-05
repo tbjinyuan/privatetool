198832d6b017202c15c3c619d4c376f93da6490d
jinyuan <jinyuan@alibaba-inc.com>
2016-09-05 21:14:08 +0800

    dev3 branch modified

--- tool-dao/src/main/java/com/zk/toolbox/dao/domain/TestUser.java
+++ tool-dao/src/main/java/com/zk/toolbox/dao/domain/TestUser.java
@@ -7,5 +7,11 @@ public class TestUser {
 
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test");
+
+        System.out.println("git test");
+
+        System.out.println("git test");
+
     }
 }
